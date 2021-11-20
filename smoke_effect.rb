291b11987c5d86b11c52d4c696b7fda8604e3fcc
nanaka0012 <nanaka0012@yahoo.co.jp>
2021-11-21 03:19:08 +0900

    update: add smoke effect

---
+++ smoke_effect.rb
@@ -0,0 +1,24 @@
+require './effect'
+require './const'
+
+class SmokeEffect < Effect
+  def initialize
+    super
+
+    @image = Gosu::Image.new('media/smoke.png')
+    @count = 0
+  end
+
+  def draw
+    super
+    x_offset = @image.width * (0.1 + 0.2 * @count) / 2
+    y_offset = @image.height * (0.1 + 0.2 * @count) / 2
+    @image.draw(@x - x_offset, @y - y_offset, Const::ZOrder::EFFECT, 0.1 + 0.2 * @count, 0.1 + 0.2 * @count)
+  end
+
+  def update
+    super
+    @count += 1
+    parent.remove_object(self) if @count == 10
+  end
+end
